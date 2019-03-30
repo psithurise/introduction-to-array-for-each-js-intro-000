f24363bbaddf14a5271752a238ebcf8a1fea2f7c
Rose Stough <psithurise@programmer.net>
2019-03-30 20:33:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Add your doToElementsInArray() function here:
 function doToElementsInArray(array, callback) {
-  array.forEach(callback);
+  array.forEach(callback)
 }
 
 // Add your changeCompletely() function here:
 function changeCompletely(element, index, array) {
-  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`
 }
 
 function doToElementsInArray(animals, changeCompletely);
